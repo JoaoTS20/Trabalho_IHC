9c5900a7d191c3a336cdb749e1e5b6f31ae4e124
Anth0nyPereira <60923555+Anth0nyPereira@users.noreply.github.com>
2020-05-16 18:38:38 +0100

    Perfil page almost finished, I'm having a bug when I upload a photo, idk why the photo does not load entirely

--- Pets_At_First_Sight/Pets_At_First_Sight/Post.xaml.cs
+++ Pets_At_First_Sight/Pets_At_First_Sight/Post.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Pets_At_First_Sight
 {
@@ -23,6 +14,23 @@ namespace Pets_At_First_Sight
         public Page1()
         {
             InitializeComponent();
+        }
+
+        public void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= TextBox_GotFocus;
+        }
+
+        private void BtnLoadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                InputImage.Source = new BitmapImage(fileUri);
+            }
         }
     }
 }
