3c14446007b6ee3bd2c7223d131a3c7d2646c520
Anth0nyPereira <60923555+Anth0nyPereira@users.noreply.github.com>
2020-05-17 16:11:11 +0100

    Perfil page almost done, need to finish the controls to show and hide information and the scroll thing

--- Pets_At_First_Sight/Pets_At_First_Sight/Perfil.xaml.cs
+++ Pets_At_First_Sight/Pets_At_First_Sight/Perfil.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows;
 
 namespace Pets_At_First_Sight
 {
@@ -10,6 +11,18 @@ namespace Pets_At_First_Sight
         public Perfil()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           rowtohide.Height = new GridLength(0);
+        }
+
+        private void More_info_button(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Infos.Visibility = Visibility.Visible;
+            rowtohide.Height = new GridLength(80);
+            Infos.Height = 100;
         }
     }
 }
