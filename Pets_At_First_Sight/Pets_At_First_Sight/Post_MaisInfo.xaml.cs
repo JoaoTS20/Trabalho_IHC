cb04e2764666cb569d274315c19980509fcd989b
Anth0nyPereira <60923555+Anth0nyPereira@users.noreply.github.com>
2020-05-18 17:52:55 +0100

    Started show perfil page and some crazy and maybe impossible ideas

---
+++ Pets_At_First_Sight/Pets_At_First_Sight/Post_MaisInfo.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pets_At_First_Sight
+{
+    /// <summary>
+    /// Interaction logic for Post_MaisInfo.xaml
+    /// </summary>
+    public partial class Post_MaisInfo : Page
+    {
+        public Post_MaisInfo()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+}
